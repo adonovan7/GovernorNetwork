31036829580d3c4d1a1234369e94ffdc50ca5cc3
Andie Donovan <andiedonovan@yahoo.com>
2017-12-04 17:28:50 -0800

    Rename GovNetwork.R to GovNetwork2.R

---
+++ GovNetwork2.R
@@ -0,0 +1,147 @@
+gov<-read.csv("/Users/andiedonovan/GovTerm.csv", header=TRUE, sep=";", stringsAsFactors = FALSE, blank.lines.skip = TRUE)
+colnames(gov)<-c("Name", "State", "Year", "Party")
+# rows<-nrow(gov)
+# cols<-ncol(gov)
+# View(gov)
+
+### Split up term dates ###
+gov$Year<- as.character(gov$Year)
+split_yr = do.call("rbind", strsplit(gov$Year, "-"))
+split_yr<- gsub("\\(", replacement="", split_yr)
+split_yr<- gsub("\\)", replacement="", split_yr)
+gov <- cbind(gov, split_yr)
+
+colnames(gov)<-c("Name", "State", "Year", "Party", "Start", "End")
+gov <- subset(gov, select = -Year )
+#gov$Term<-NA
+#gov$Term <- interval(as.Date(gov$End, format='%Y'), as.Date(gov$Start, format='%Y'))
+
+gov$Start <- as.character(gov$Start)
+gov$End <- as.character(gov$End)
+gov$Start <- as.numeric(gov$Start)
+gov$End <- as.numeric(gov$End)
+max(gov$End - gov$Start) # 16 year term
+gov$Group<-NA
+gov[,7:22]<-NA
+
+gov$Start <- as.character(gov$Start)
+gov$End <- as.character(gov$End)
+gov$Start <- as.numeric(gov$Start)
+gov$End <- as.numeric(gov$End)
+for (i in 1:nrow(gov)){
+  myseq <- seq(gov[i,4], gov[i,5], by=1)
+  for (j in 1:length(myseq)){
+    a = 6+j
+    gov[i,a] <- myseq[j]
+  }
+}
+
+# creating columns with year # of full term in office
+names(gov)[7:23] <- paste("Year", seq(1, 16, by=1), sep="")
+
+period1 <- seq(from=1775, to=1824, by =1)
+period2 <- seq(from=1825, to=1874, by =1)
+period3 <- seq(from=1875, to=1924, by =1)
+period4 <- seq(from=1925, to=1974, by =1)
+period5 <- seq(from=1975, to=2025, by =1)
+
+### splitting into periods 1, 2, 3, 4, or 5
+for (j in 1:nrow(gov)){ 
+  for (i in 7:22){
+    if (gov[j,i] %in% period1 & is.na(gov[j,6])==TRUE){
+      gov[j,6] <- "1"
+    }
+    else if (gov[j,i] %in% period2 & is.na(gov[j,6])==TRUE){
+      gov[j,6]<- "2"
+    }
+    else if (gov[j,i] %in% period3 & is.na(gov[j,6])==TRUE){
+      gov[j,6]<- "2"
+    }
+    else if (gov[j,i] %in% period3 & is.na(gov[j,6])==TRUE){
+      gov[j,6]<- "3"
+    }
+    else if (gov[j,i] %in% period4 & is.na(gov[j,6])==TRUE){
+      gov[j,6]<- "4"
+    }
+    else if (gov[j,i] %in% period5 & is.na(gov[j,6])==TRUE){
+      gov[j,6]<- "5"
+    }
+  }
+}
+
+# make data frames for each group of years
+gov$Group<- as.numeric(gov$Group)
+#for (i in 1:5){
+#  paste("group", i, sep="") <- filter(gov, Group==i)
+#}
+
+#group1<-filter(gov, Group==1)
+#group2<-filter(gov, Group==2)
+#group3<-filter(gov, Group==3)
+#group4<-filter(gov, Group==4)
+#group5<-filter(gov, Group==5)
+
+#Group<- c(group1, group2, group3, group4, group5)
+
+##########################
+# Create network matrix #
+##########################
+#View(gov)
+#class(gov$Group)
+#filter(gov, Group==2)
+
+#for (x in 1:5){
+x=2  
+  y <- gov[gov$Group==x,] ### need to filter
+  View(y)
+  gov.mat<-as.matrix(y) # turn the table into a matrix object
+  gov.mat2<-as.matrix(y) # create dummy copy for comparison
+  rows<-nrow(y)
+  cols<-ncol(y)
+  View(gov.mat)
+  adj<-matrix(0, nrow=rows, ncol=rows) # empty matrix; adjacency matrix
+  gov.net<-network.initialize(rows) # create network object
+  
+  #check if same state
+  for (i in 1:rows){
+    for (j in 1:rows){
+      if (gov.mat[i,2] == gov.mat2[j,2]) {  
+        if (i==j){
+          adj[i,j] = adj[i,j]
+        }
+        else if (i!=j){
+          adj[i,j] = adj[i,j]+1
+        }
+      }
+    }
+  }
+  
+  ## seeing which politicians are of the same party
+  for (i in 1:rows){
+    for (j in 1:rows){
+      if (gov.mat[i,3] == gov.mat2[j,3]){
+        if (i==j){
+          adj[i,j] = adj[i,j]
+        }
+        else if (i!=j){
+          adj[i,j] = adj[i,j]+1
+        }
+      }
+    }
+  }
+  
+  mib<-network.initialize(nrow(adj))
+  mib[adj==1] <- 1
+  mib[adj==2] <- 2
+  plot(mib)
+  
+ # }
+
+
+################ 
+# network object 
+################
+mib<-network.initialize(nrow(adj))
+mib[adj==1] <- 1
+mib[adj==2] <- 2
+plot(mib)
